c49e93201411e8a353644c15a74d4bc0bded77d3
mgarnier59 <50111289+mgarnier59@users.noreply.github.com>
2025-03-06 13:38:52 +0100

    add support for manual coloring

--- veriphix/client.py
+++ veriphix/client.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections.abc import Sequence
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
@@ -12,16 +13,16 @@ import graphix.sim.statevec
 import graphix.simulator
 import networkx as nx
 import numpy as np
+import stim
 from graphix.clifford import Clifford
 from graphix.command import BaseM, CommandKind, MeasureUpdate
 from graphix.measurements import Measurement
 from graphix.ops import Ops
 from graphix.pattern import Pattern
 from graphix.pauli import Pauli
-from graphix.sim.statevec import StatevectorBackend, Statevec
+from graphix.sim.statevec import Statevec, StatevectorBackend
 from graphix.simulator import MeasureMethod, PatternSimulator
 from graphix.states import BasicStates
-import stim
 
 from veriphix.trappifiedCanvas import TrappifiedCanvas
 
@@ -72,7 +73,7 @@ class SecretDatas:
 
     # NOTE: not a class method?
     @staticmethod
-    def from_secrets(secrets: Secrets, graph:nx.Graph, input_nodes, output_nodes):
+    def from_secrets(secrets: Secrets, graph: nx.Graph, input_nodes, output_nodes):
         nodes, edges = graph
         # Re-write graph
         graph = nx.Graph()
@@ -101,7 +102,7 @@ class SecretDatas:
             # After all the `a` secrets have been generated, the `a_N` value can be
             # computed from the graph topology
             for i in graph.nodes:
-                a_N_value = sum([a[j] for j in graph.neighbors(i)])%2
+                a_N_value = sum([a[j] for j in graph.neighbors(i)]) % 2
                 # for j in graph.neighbors(i):
                 #     a_N_value ^= a[j]
                 a_N[i] = a_N_value
@@ -159,7 +160,7 @@ def prepared_nodes_as_input_nodes(pattern: Pattern) -> Pattern:
 
 class Client:
     def __init__(self, pattern, input_state=None, measure_method_cls=None, secrets: None | Secrets = None) -> None:
-        self.initial_pattern:Pattern = pattern
+        self.initial_pattern: Pattern = pattern
 
         self.input_nodes = self.initial_pattern.input_nodes.copy()
         self.output_nodes = self.initial_pattern.output_nodes.copy()
@@ -178,9 +179,8 @@ class Client:
         pattern_copy = Pattern(pattern.input_nodes)
         for cmd in pattern:
             pattern_copy.add(cmd)
-        
+
         self.measurement_db = {measure.node: measure for measure in pattern_copy.get_measurement_commands()}
-        
 
         self.byproduct_db = get_byproduct_db(pattern_copy)
 
@@ -193,7 +193,6 @@ class Client:
             secrets = Secrets()
 
         self.secret_datas = SecretDatas.from_secrets(secrets, self.graph, self.input_nodes, self.output_nodes)
-
 
         # pattern_without_flow = remove_flow(pattern)
         # self.clean_pattern = prepared_nodes_as_input_nodes(pattern_without_flow)
@@ -224,12 +223,12 @@ class Client:
         #         if theta:
         #             backend.apply_single(node=node, op=z_rotation(theta))
 
-    def prepare_states_virtual(self, backend:Backend) -> None:
-        for node in self.nodes_list :
+    def prepare_states_virtual(self, backend: Backend) -> None:
+        for node in self.nodes_list:
             if node in self.input_nodes:
                 state = self.input_state[node]
 
-            elif node in self.output_nodes :
+            elif node in self.output_nodes:
                 r_value = self.secret_datas.r.get(node, 0)
                 a_N_value = self.secret_datas.a.a_N.get(node, 0)
                 state = BasicStates.PLUS if r_value ^ a_N_value == 0 else BasicStates.MINUS
@@ -238,12 +237,13 @@ class Client:
                 state = BasicStates.PLUS
             self.blind_qubit(backend=backend, node=node, state=state)
 
-    def blind_qubit(self, backend:Backend, node:int, state) -> None:
+    def blind_qubit(self, backend: Backend, node: int, state) -> None:
         def z_rotation(theta) -> np.array:
             return np.array([[1, 0], [0, np.exp(1j * theta * np.pi / 4)]])
 
         def x_blind(a) -> Pauli:
             return Pauli.X if (a == 1) else Pauli.I
+
         theta = self.secret_datas.theta.get(node, 0)
         a = self.secret_datas.a.a.get(node, 0)
         single_qubit_backend = StatevectorBackend()
@@ -260,10 +260,9 @@ class Client:
         self.prepare_states_virtual(backend=backend)
         # Server asks the backend to create them
         ## Except for the input!
-        backend.add_nodes(nodes = self.input_nodes)
+        backend.add_nodes(nodes=self.input_nodes)
 
-
-    def create_test_runs(self) -> list[TrappifiedCanvas]:
+    def create_test_runs(self, manual_colouring: Sequence[set[int]] | None = None) -> list[TrappifiedCanvas]:
         """
         Creates test runs according to FK12 protocol of
         Fitzsimons, J. F., & Kashefi, E. (2017). Unconditionally verifiable blind quantum computation. Physical Review A, 96(1), 012303.
@@ -275,19 +274,22 @@ class Client:
         For a color, single-qubit traps are created for each node of that color.
         """
 
-        # TODO: just pattern.get_graph()
         graph = nx.Graph()
         nodes, edges = self.graph
         graph.add_edges_from(edges)
         graph.add_nodes_from(nodes)
 
         # Create the graph coloring
-        coloring = nx.coloring.greedy_color(graph, strategy="largest_first")
-        colors = set(coloring.values())
-        nodes_by_color = {c: [] for c in colors}
-        for node in sorted(graph.nodes):
-            color = coloring[node]
-            nodes_by_color[color].append(node)
+        # Networkx output format: dict[int, int] eg {0: 0, 1: 1, 2: 0, 3: 1}
+        if manual_colouring is None:
+            coloring = nx.coloring.greedy_color(graph, strategy="largest_first")
+            colors = set(coloring.values())
+            nodes_by_color = {c: [] for c in colors}
+            for node in sorted(graph.nodes):
+                color = coloring[node]
+                nodes_by_color[color].append(node)
+        else:
+            nodes_by_color = {i: list(c) for i, c in enumerate(manual_colouring)}
 
         # Create the test runs : one per color
         runs: list[TrappifiedCanvas] = []
@@ -323,7 +325,6 @@ class Client:
                 backend.add_nodes(nodes=[node], data=state)
         # The backend knows what state to create when needed
 
-
         # backend.add_nodes(nodes=sorted(self.graph[0]), data=preparation_backend.state)
 
         tmp_measurement_db = self.measurement_db.copy()
@@ -350,9 +351,8 @@ class Client:
 
         return trap_outcomes
 
-
     def delegate_pattern(self, backend: Backend, **kwargs) -> None:
-        # Initializes the bank & asks backend to create the input 
+        # Initializes the bank & asks backend to create the input
         self.prepare_states(backend)
         # self.blind_qubits(backend)
 
@@ -387,8 +387,8 @@ class Client:
         return z_decoding, x_decoding
 
 
-class CircuitUtils():
-    def tableau_to_pattern(tableau:stim.Tableau)-> graphix.Pattern :
+class CircuitUtils:
+    def tableau_to_pattern(tableau: stim.Tableau) -> graphix.Pattern:
         n = len(tableau)
         circuit = tableau.to_circuit()
         graphix_circuit = graphix.Circuit(n)
