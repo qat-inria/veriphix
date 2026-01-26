cc7b08f4cdcc8e6070d5308fe73beb670d30e96c
Sami AS <samiabdulsater@gmail.com>
2026-01-26 15:57:05 +0100

    Format

--- veriphix/sampling_circuits/qasm_parser.py
+++ veriphix/sampling_circuits/qasm_parser.py
@@ -4,7 +4,10 @@ import ast
 import math
 import operator
 import re
-from typing import TYPE_CHECKING, Callable
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
 
 from graphix import Circuit
 
