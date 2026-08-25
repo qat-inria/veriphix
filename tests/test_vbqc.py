9108f6be0a213850d488cc087f2e1c0383034490
jemappellesami <44751925+jemappellesami@users.noreply.github.com>
2026-08-25 18:29:25 -0400

    Update tests/test_vbqc.py
    Co-authored-by: thierry-martinez <thierry.martinez@inria.fr>

--- tests/test_vbqc.py
+++ tests/test_vbqc.py
@@ -180,7 +180,7 @@ class TestVBQC:
             )
         elif noise_model_name == "malicious":
             subset_size = fx_rng.integers(1, len(client.nodes))
-            malicious_nodes = [int(node) for node in fx_rng.choice(client.nodes, size=subset_size, replace=False)]
+            malicious_nodes = fx_rng.choice(client.nodes, size=subset_size, replace=False)
 
             noise_model = MaliciousNoiseModel(
                 nodes=malicious_nodes,
